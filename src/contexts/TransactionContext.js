4cc546bdf937ec6990c016d6573b89fbe6e57fc5
Gage <gagemigan@gmail.com>
2020-09-09 22:38:28 -0400

    setup basic context architecture for TransactionContext

--- src/contexts/TransactionContext.js
+++ src/contexts/TransactionContext.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 const nullIncomeInfo = {
-
+    
+        "id": null,
+        "user_id": null,
+        "name": null,
+        "income_amount": null,
+        "transaction_category": null,
+        "date_created": null
+    
 };
 
 const nullExpensesInfo = {
-
+    "id": null,
+    "user_id": null,
+    "name": null,
+    "expense_amount": null,
+    "expense_category": null,
+    "date_created": null
 };
 
 const TransactionContext = React.createContext({
@@ -22,4 +34,19 @@ export class TransactionProvider extends React.Component{
         incomeInfo : nullIncomeInfo,
         expensesInfo : nullExpensesInfo,
     }
+
+
+    render(){
+        const value = {
+            error : this.state.error,
+            incomeInfo : this.state.incomeInfo,
+            expensesInfo : this.state.expensesInfo,
+        }
+
+        return(
+            <TransactionContext.Provider value={value}>
+                {this.props.children}
+            </TransactionContext.Provider>
+        )
+    };
 }
