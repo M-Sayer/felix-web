522d29a231bedc8d4d05117d56c149136b17e288
Gage <gagemigan@gmail.com>
2020-09-09 22:47:00 -0400

    forgot to save on transaction route... :|

---
+++ src/routes/transactionFullInfo/transactionFullInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//context_goes_here\
+
+export default class TransactionFullInfo extends React.Component{
+    static 
+
+    /*static contextType = context*/
+
+    render(){
+        return(
+            <section>
+
+            </section>
+        );
+    }
+}
