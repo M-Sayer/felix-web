f50352fbdb7553c0d8bc6e2f0b778a272d9ed5a3
Gage <gagemigan@gmail.com>
2020-09-15 12:04:07 -0400

    added edit api call function

--- src/services/transactions-api-service.js
+++ src/services/transactions-api-service.js
@@ -9,6 +9,20 @@ const TransactionApiService = {
                 :res.json()
                 )
     },
+    updateSingleTransaction( type, id, content){
+        return fetch(`${config.API_ENDPOINT}/transactions/${type}/${id}`, {
+            method : 'PATCH',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify(content)
+        })
+        .then(res => 
+            (!res.ok)
+            ?res.json().then(e => Promise.reject(e))
+            :res.json()
+            )
+    }
 };
 
 
