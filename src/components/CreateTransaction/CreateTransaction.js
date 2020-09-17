4970d6d603f8509b60d52e16f11c2d5146f4d6fb
jenthura <joshuasth@gmail.com>
2020-09-17 13:44:50 -0500

    created createTransaction component

--- src/components/CreateTransaction/CreateTransaction.js
+++ src/components/CreateTransaction/CreateTransaction.js
@@ -1,1 +1,42 @@
 import React from 'react';
+import TransactionForm from '../TransactionForm/TransactionForm'
+import TransactionsService from '../../services/transactions-service'
+import TransactionsContext from '../../contexts/TransactionsContext';
+
+
+export default class CreateTransaction extends React.Component {
+	static contextType = TransactionsContext;
+
+
+	componentDidMount = () => {
+		this.context.clearTransactionForm();      
+	}
+
+	handleSubmit = ev => {
+			ev.preventDefault();
+			this.context.setTransaction(this.context.transactionForm)
+			console.log(this.context.transactionForm)
+			TransactionsService.createTransaction(this.context.transactionForm);
+	}
+
+	handleChange = ev =>{
+			ev.preventDefault();
+			const {name , value} = ev.target;
+			this.context.setTransactionFormChange(name, value)
+	}
+
+	handleCancel = ev => {
+			ev.preventDefault();
+			//close the create form somehow
+	}
+
+	render() {
+		return (
+			<TransactionForm
+			handleCancel = {this.handleCancel}
+			handleChange = {this.handleChange}
+			handleSubmit = {this.handleSubmit}
+			editing = {false}/>
+		);
+	};
+};
