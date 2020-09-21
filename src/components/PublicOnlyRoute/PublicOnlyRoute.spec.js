968fb99a1864417e7a377ce92658d350f5b0b9a9
mischkiey <mischkieycf@gmail.com>
2020-09-21 12:11:22 -0400

    Revert "Added tests for all routes. Fixed bug causing PublicOnlyRoute smoke test to fail"
    This reverts commit 92e43f13f084d93363627cd3e4be689c196ed2c6.

--- src/components/PublicOnlyRoute/PublicOnlyRoute.spec.js
+++ src/components/PublicOnlyRoute/PublicOnlyRoute.spec.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-// import renderer from 'react-test-renderer';
-import { BrowserRouter } from 'react-router-dom';
-import PublicOnlyRoute from './PublicOnlyRoute';
-import DashboardRoute from '../../routes/DashboardRoute'
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import renderer from 'react-test-renderer';
+// import { BrowserRouter } from 'react-router-dom';
+// import PublicOnlyRoute from './PublicOnlyRoute';
 
-describe(`PublicOnlyRoute Component`, () => {
-  describe(`Smoke test`, () => {
-    it(`Renders without crashing`, () => {
-      const div = document.createElement('div');
-      ReactDOM.render(
-        <BrowserRouter>
-          <PublicOnlyRoute
-            comp={DashboardRoute}
-          />
-        </BrowserRouter>,
-        div);
-      ReactDOM.unmountComponentAtNode(div);
-    });
-  });
+// describe(`PublicOnlyRoute Component`, () => {
+//   describe(`Smoke test`, () => {
+//     it(`Renders without crashing`, () => {
+//       const div = document.createElement('div');
+//       ReactDOM.render(
+//         <BrowserRouter>
+//           <PublicOnlyRoute />
+//         </BrowserRouter>,
+//         div);
+//       ReactDOM.unmountComponentAtNode(div);
+//     });
+//   });
   
-  // describe(`Snapshot test`, () => {
-  //   it(`Renders the UI as expected`, () => {
-  //     const tree = renderer
-  //       .create(
-  //         <BrowserRouter>
-  //             <App />
-  //         </BrowserRouter>
-  //       )
-  //       .toJSON()
-  //     expect(tree).toMatchSnapshot()
-  //   });
-  // });
-});
+//   // describe(`Snapshot test`, () => {
+//   //   it(`Renders the UI as expected`, () => {
+//   //     const tree = renderer
+//   //       .create(
+//   //         <BrowserRouter>
+//   //             <App />
+//   //         </BrowserRouter>
+//   //       )
+//   //       .toJSON()
+//   //     expect(tree).toMatchSnapshot()
+//   //   });
+//   // });
+// });
