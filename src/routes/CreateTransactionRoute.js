1e1ae70b4e896f024c1566746a19b26117e0e6b5
mischkiey <mischkieycf@gmail.com>
2020-09-21 11:23:13 -0400

    Fixed transaction form for code consistency

--- src/routes/CreateTransactionRoute.js
+++ src/routes/CreateTransactionRoute.js
@@ -7,7 +7,9 @@ export default class CreateTransactionRoute extends React.Component {
     return (
       <>
         <section>
-          <CreateTransaction history={this.props.history} />
+          <CreateTransaction
+            history={this.props.history}
+          />
         </section>
       </>
     );
