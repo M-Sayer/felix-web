488b595c29a38b3317ca9d5b413116c43f05cb56
M-Sayer <haji.san@icloud.com>
2020-09-25 13:02:13 -0400

    styling for alerts

--- src/routes/DashboardRoute.js
+++ src/routes/DashboardRoute.js
@@ -4,40 +4,19 @@ import GoalsOverview from '../components/Overview/GoalsOverview';
 import TransactionsOverview from '../components/Overview/TransactionsOverview';
 import Alerts from '../components/Alerts/Alerts';
 import AlertsContext from '../contexts/AlertsContext';
-import { useHistory } from 'react-router-dom';
-
 
 const DashboardRoute = (props) => {
   const alertsContext = useContext(AlertsContext);
-  const history = useHistory();
 
   return (
     <>
-    <section
-      className='AlertsOverview'
-    >
-      <article 
-        className='overviewSection'
-      >
-        <button
-          className='AlertsButton btn tertiaryBtn'
-          onClick={() => 
-            history.push('/alerts')
-          }
-        >
-          alerts
-        </button>
-        {alertsContext.dashboardAlerts.length > 0 &&
-          <section
-          className='Alerts'
-          >
-            <Alerts />
-          </section>
-        }
-
-      </article> 
-
-    </section>
+    {alertsContext.dashboardAlerts.length > 0 &&  
+      <section className='AlertsOverview'>       
+        <section className='Alerts'>
+          <Alerts />
+        </section>
+      </section>
+    }
   
       <section
         className='UserOverview'
